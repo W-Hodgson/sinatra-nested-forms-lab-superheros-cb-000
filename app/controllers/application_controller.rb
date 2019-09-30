0a47274b426c88f7ce494ad7f55c817f19113b38
Will Hodgson <will.hodgson@current-rms.com>
2019-09-30 09:38:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     post '/team' do
       @team = Team.new(params[:team])
       params[:team][:heroes].each do |hero|
-        Hero.new()
+        Hero.new(hero)
       end
       erb :team
     end
